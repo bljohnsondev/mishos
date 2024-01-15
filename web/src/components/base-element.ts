1e2e1829cbb46395b2703289852951fdbd53a7ff
Brent Johnson <13071953+bljohnsondev@users.noreply.github.com>
2024-01-14 23:14:35 -0600

    Added BaseElement

---
+++ web/src/components/base-element.ts
@@ -0,0 +1,14 @@
+import { LitElement } from 'lit';
+
+import { ToastMessage } from '@/types';
+import { createEvent, createToastEvent } from '@/utils';
+
+export class BaseElement extends LitElement {
+  toast(message: ToastMessage) {
+    this.dispatchEvent(createToastEvent(message));
+  }
+
+  dispatchCustomEvent(name: string, detail?: any) {
+    createEvent(name, detail);
+  }
+}
