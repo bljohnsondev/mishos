700cb1f3728ed8ceb3f614c31107dba00a047a98
Brent Johnson <13071953+bljohnsondev@users.noreply.github.com>
2024-01-28 01:47:58 -0600

    Added API endpoint for show export

--- api/src/init/init.module.ts
+++ api/src/init/init.module.ts
@@ -3,14 +3,14 @@ import { JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AuthService } from '@/auth/auth.service';
-import { User, UserConfig } from '@/common/entity';
+import { FollowedShow, Show, User, UserConfig, WatchedEpisode } from '@/common/entity';
 import { ShowModule } from '@/show/show.module';
 
 import { InitController } from './init.controller';
 import { InitService } from './init.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User, UserConfig]), ShowModule],
+  imports: [TypeOrmModule.forFeature([FollowedShow, Show, User, UserConfig, WatchedEpisode]), ShowModule],
   controllers: [InitController],
   providers: [AuthService, InitService, JwtService],
 })
--- api/src/init/init.service.ts
+++ api/src/init/init.service.ts
@@ -1,16 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import bcrypt from 'bcrypt';
-import { DataSource } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 
 import { AuthService } from '@/auth/auth.service';
 import { UserDto } from '@/auth/dto';
 import { UserConfigDto } from '@/common/dto';
-import { UserConfig } from '@/common/entity';
+import { FollowedShow, Show, WatchedEpisode, UserConfig } from '@/common/entity';
 import { ShowException } from '@/common/exceptions';
 
 @Injectable()
 export class InitService {
-  constructor(private authService: AuthService, private dataSource: DataSource) {}
+  constructor(
+    private authService: AuthService,
+    private dataSource: DataSource,
+    @InjectRepository(FollowedShow) private followedShowRepository: Repository<FollowedShow>,
+    @InjectRepository(Show) private showRepository: Repository<Show>,
+    @InjectRepository(WatchedEpisode) private watchedRepository: Repository<WatchedEpisode>
+  ) {}
 
   async findUserConfig(authUser: UserDto): Promise<UserConfig> {
     const user = await this.authService.findUserById(authUser.id);
@@ -46,6 +53,92 @@ export class InitService {
   }
 
   async generateExportData(authUser: UserDto): Promise<any> {
-    return { userId: authUser.id, message: 'This is a test' };
+    const user = await this.authService.findUserById(authUser.id);
+    if (!user) throw new ShowException('User not found');
+
+    const followedShows = await this.followedShowRepository.find({
+      select: {
+        show: {
+          id: true,
+          name: true,
+          providerId: true,
+        },
+      },
+      where: {
+        user: {
+          id: user.id,
+        },
+      },
+      relations: {
+        show: true,
+      },
+      order: {
+        show: {
+          name: 'ASC',
+        },
+      },
+    });
+
+    const allWatched = await this.watchedRepository.find({
+      select: {
+        episode: {
+          providerId: true,
+          name: true,
+          number: true,
+          season: {
+            number: true,
+            show: {
+              id: true,
+              name: true,
+            },
+          },
+        },
+      },
+      where: {
+        user: {
+          id: user.id,
+        },
+      },
+      relations: {
+        episode: {
+          season: {
+            show: true,
+          },
+        },
+      },
+      order: {
+        episode: {
+          season: {
+            number: 'ASC',
+          },
+          number: 'ASC',
+        },
+      },
+    });
+
+    const exportedShows = followedShows.map(followed => {
+      const episodes = allWatched
+        .filter(watched => {
+          return watched.episode?.season?.show?.id === followed.show.id;
+        })
+        .map(ep => ({
+          id: ep.id,
+          providerId: ep.episode.providerId,
+          name: ep.episode.name,
+          seasonNumber: ep.episode.season?.number,
+          episodeNumber: ep.episode.number,
+        }));
+
+      return {
+        id: followed.show.id,
+        providerId: followed.show.providerId,
+        name: followed.show.name,
+        episodes,
+      };
+    });
+
+    return {
+      shows: exportedShows,
+    };
   }
 }
