90091965c108bb88b41dc1328c1514b26453a337
Brent Johnson <13071953+bljohnsondev@users.noreply.github.com>
2024-11-26 00:42:24 -0600

    Removed frontend manual timezone conversion

--- web/src/utils/date-utils.ts
+++ web/src/utils/date-utils.ts
@@ -1,26 +1,13 @@
 import dayjs from 'dayjs';
-import timezone from 'dayjs/plugin/timezone';
-import utc from 'dayjs/plugin/utc';
-
-dayjs.extend(utc);
-dayjs.extend(timezone);
-
-const TIMEZONE = import.meta.env.VITE_TIMEZONE ?? 'UTC';
 
 export const formatDate = (date?: Date, format?: string) => {
-  return date
-    ? dayjs(date)
-        .tz(TIMEZONE)
-        .format(format ?? 'MM-DD-YYYY')
-    : null;
+  return date ? dayjs(date).format(format ?? 'MM-DD-YYYY') : null;
 };
 
 export const formatDateTime = (date?: Date) => {
-  return date ? dayjs(date).tz(TIMEZONE).format('MM-DD-YYYY HH:mm') : null;
+  return date ? dayjs(date).format('MM-DD-YYYY HH:mm') : null;
 };
 
 export const formatAirTime = (date?: Date) => {
-  return date ? dayjs(date).tz(TIMEZONE).format('MM-DD-YYYY [·] h:mm a') : null;
+  return date ? dayjs(date).format('MM-DD-YYYY [·] h:mm a') : null;
 };
-
-export { TIMEZONE as timezone };
